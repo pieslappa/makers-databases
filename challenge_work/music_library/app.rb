40acdeb9096c8abacafe6e02150f3e6b0a86a938
Chris Leech <chrisleech@monzo.com>
2023-02-03 10:42:19 +0000

    added application layer

--- challenge_work/music_library/app.rb
+++ challenge_work/music_library/app.rb
@@ -1,23 +1,53 @@
-require_relative 'lib/database_connection'
-require_relative 'lib/album_repository.rb'
-require_relative 'lib/artist_repository.rb'
+require_relative './lib/database_connection.rb'
+require_relative './lib/album_repository.rb'
+require_relative './lib/artist_repository.rb'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+class Application
+  # The Application class initializer
+  # takes four arguments:
+  #  * The database name to call `DatabaseConnection.connect`
+  #  * the Kernel object as `io` (so we can mock the IO in our tests)
+  #  * the AlbumRepository object (or a double of it)
+  #  * the ArtistRepository object (or a double of it)
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
 
-# Perform a SQL query on the database and get the result set.
-album_repository = AlbumRepository.new
-artist_repository = ArtistRepository.new
+  def run
+    @io.puts "What would you like to do?"
+    @io.puts "1 - List all albums"
+    @io.puts "2 - List all arists"
 
-# Print out each record from the result set .
-# album_repository.all.each do |album|
-#   p album
-# end
+    @io.puts "Enter your choice:"
+    choice = @io.gets.chomp
 
-# artist_repository.all.each do |artist|
-#   p artist
-# end
+    if choice == "1"
+      puts "Here is the list of albums:"
+      albums = @album_repository.all
+      albums.each do |album|
+        puts "* #{album.id} - #{album.title}"
+      end
+    elsif choice == "2"
+      artists = @artist_repository.all
+      puts "Here is the list of artists:"
+      artists.each do |artist|
+        puts "* #{artist.id} - #{artist.name}"
+      end
+    else
+      p "Choice not recognised"
+    end
+  end
+end
 
-
-
-p album_repository.find(3)
+if __FILE__ == $0
+  app = Application.new(
+    'music_library',
+    Kernel,
+    AlbumRepository.new,
+    ArtistRepository.new
+  )
+  app.run
+end
